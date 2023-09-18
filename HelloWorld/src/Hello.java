f047b8a91561d3a65109cccf68c67711eb7bcdbc
Ian McBee <mcbee_ian@bah.com>
2023-09-18 15:16:51 -0400

    trying to blow through basics

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -23,5 +23,47 @@ public class Hello {
       System.out.println("Either or both of the conditions are true");
     }
 
+    int newValue = 50;
+    if (newValue == 50) {
+      System.out.println("This is an true");
+    }
+
+    boolean isCar = false;
+    if (!isCar) {
+      System.out.println("This is not supposed to happen.");
+    }
+
+    String makeOfCar = "Volkswagen";
+    boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+    if (isDomestic) {
+      System.out.println("This car is domestic to our country");
+    }
+
+    String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+    System.out.println(s);
+
+    //? Challenge
+    double value1 = 20.00d;
+    double value2 = 80.00d;
+    double added = (value1 + value2) * 100.00d;
+    double remainderOfAdded = added % 40.00d;
+    boolean isTrue;
+
+//    if (remainderOfAdded == 0.00) {
+//      isTrue = true;
+//    } else {
+//      isTrue = false;
+//    }
+
+    isTrue = remainderOfAdded == 0.00; //! Same thing as above
+
+    System.out.println(isTrue);
+    if (!isTrue) {
+      System.out.println("got some remainder");
+    }
+
+
+
+
   }
 }
