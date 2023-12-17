34fb6a619e6294c2742025f3c54f2583b8bde659
Ian McBee <mcbee_ian@bah.com>
2023-12-17 14:32:07 -0500

    update

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -37,20 +37,19 @@ public class Main {
 
     //! Code Blocks
     boolean gameOver = true;
-    int score = 5000;
+    int score = 4000;
     int levelCompleted = 5;
     int bonus = 100;
 
-    if (score == 5000) {
-      System.out.println("Your score was 5000");
-    }
 
-    if (score <= 5000) {
+    if (score < 5000 && score > 1000) {
       System.out.println("Your score was less than or equal to 5000");
+    } else if (score < 1000) {
+      System.out.println("Your score was less than 1000.");
     } else {
       System.out.println("Got here");
     }
 
-    
+
   }
 }
